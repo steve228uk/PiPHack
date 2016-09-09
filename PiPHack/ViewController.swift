53e44e1e98213e131fd443479ca2fabeb8a74b9f
Stephen Radford <steve228uk@gmail.com>
2016-09-09 23:08:54 +0100

    Initial Commit

---
+++ PiPHack/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PiPHack
+//
+//  Created by Stephen Radford on 09/09/2016.
+//  Copyright © 2016 Cocoon Development Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
